ebd2b0ec8b04f6ab33f5345adad50a5df8136152
Mike Echevarria <mike.echevarria@gmail.com>
2022-06-05 10:06:36 -0400

    properly name timestamp variable and minor formatting consistency

--- script.py
+++ script.py
@@ -11,7 +11,6 @@ client_secret = os.getenv('CLIENT_SECRET')
 webhook_url = os.getenv('WEBHOOK_URL')
 
 print('Authenticating to Twitch')
-
 auth_params = {'client_id': client_id, 'client_secret': client_secret, 'grant_type' : 'client_credentials'}
 auth_response = requests.post('https://id.twitch.tv/oauth2/token', data=auth_params)
 
@@ -21,13 +20,13 @@ print('Getting stream info')
 headers= {'Authorization' : f'Bearer {access_token}', 'Client-Id' : client_id}
 stream_params = {'user_login': twitch_user}
 response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=stream_params)
+
 data = response.json()['data'][0]
 
 print('Posting message to Discord')
 game_name = data['game_name']
 title = data['title']
-random = time.time_ns()
-
+timestamp = time.time_ns()
 discord_body = {
   'content': f'@everyone {twitch_user} is now LIVE on Twitch! See you in chat! :rocket:',
   'embeds': [
@@ -36,7 +35,7 @@ discord_body = {
       'url': f'https://www.twitch.tv/{twitch_user}',
       'color': 9174960,
       'image': {
-        'url': f'https://static-cdn.jtvnw.net/previews-ttv/live_user_{twitch_user}-320x180.jpg?rnd={random}'
+        'url': f'https://static-cdn.jtvnw.net/previews-ttv/live_user_{twitch_user}-320x180.jpg?ts={timestamp}'
       },
       'fields': [
         {
